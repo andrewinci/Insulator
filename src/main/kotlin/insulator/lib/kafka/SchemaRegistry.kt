5de5899ec9a011dacf2d3616a146153ae7436274
darka91 <andrewinci@gmail.com>
2020-07-19 22:48:39 +0100

    Sort schemas

--- src/main/kotlin/insulator/lib/kafka/SchemaRegistry.kt
+++ src/main/kotlin/insulator/lib/kafka/SchemaRegistry.kt
@@ -3,5 +3,5 @@ package insulator.lib.kafka
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient
 
 class SchemaRegistry(private val client: SchemaRegistryClient){
-    fun getAllSubjects() : Collection<String> = client.allSubjects
+    fun getAllSubjects() : Collection<String> = client.allSubjects.sorted()
 }
