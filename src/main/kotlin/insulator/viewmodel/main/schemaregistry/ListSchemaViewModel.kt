7ed72e574bca5aa3576a54321c388eadefaa6480
darka91 <andrewinci@gmail.com>
2020-09-12 07:42:52 +0100

    Improve error handling in schemaView

--- src/main/kotlin/insulator/viewmodel/main/schemaregistry/ListSchemaViewModel.kt
+++ src/main/kotlin/insulator/viewmodel/main/schemaregistry/ListSchemaViewModel.kt
@@ -4,20 +4,22 @@ import arrow.core.extensions.either.applicativeError.handleError
 import insulator.lib.helpers.runOnFXThread
 import insulator.lib.kafka.SchemaRegistry
 import insulator.viewmodel.common.InsulatorViewModel
+import insulator.views.main.schemaregistry.SchemaView
+import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.collections.ObservableList
+import tornadofx.*
 
 class ListSchemaViewModel : InsulatorViewModel() {
 
     private val schemaRegistryClient: SchemaRegistry by di()
 
+    val selectedSchema = SimpleStringProperty()
     val listSchema: ObservableList<String> = FXCollections.observableArrayList<String>()
 
-    init { refresh() }
-
-    fun getSchema(subject: String) = schemaRegistryClient.getSubject(subject)
-        .map { SchemaViewModel(it) }
-        .fold({ throw it }, { it })
+    init {
+        refresh()
+    }
 
     fun refresh() = schemaRegistryClient
         .getAllSubjects()
@@ -28,6 +30,24 @@ class ListSchemaViewModel : InsulatorViewModel() {
                 listSchema.addAll(it)
             }
         }.handleError {
-            error.set(it)
+            error.set(LoadSchemaListError(it.message ?: "Unable to load the schema list"))
         }
+
+    fun showSchema() {
+        if (selectedSchema.value.isNullOrEmpty()) return
+        schemaRegistryClient.getSubject(selectedSchema.value!!)
+            .map { SchemaViewModel(it) }
+            .fold(
+                { error.set(LoadSchemaError(it.message ?: "Unable to load the schema")) },
+                {
+                    val scope = Scope()
+                    tornadofx.setInScope(it, scope)
+                    find<SchemaView>(scope)
+                        .also { view -> view.whenUndockedOnce { refresh() } }
+                        .openWindow()
+                })
+    }
 }
+
+class LoadSchemaError(message: String) : Throwable(message)
+class LoadSchemaListError(message: String) : Throwable(message)
