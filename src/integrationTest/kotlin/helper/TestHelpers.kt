2d4d155071c41d5c531b73420d132a3fd0ad6150
darka91 <andrewinci@gmail.com>
2020-09-19 12:05:24 +0100

    Improve sample test

--- src/integrationTest/kotlin/helper/TestHelpers.kt
+++ src/integrationTest/kotlin/helper/TestHelpers.kt
@@ -1,17 +1,31 @@
 package helper
 
+import javafx.stage.Stage
 import org.testfx.api.FxToolkit
 import tornadofx.* // ktlint-disable no-wildcard-imports
 import kotlin.reflect.KClass
 
-fun configureDi(vararg dependencyMap: Pair<KClass<*>, Any>) {
+fun configureIntegrationDi(vararg dependencyMap: Pair<KClass<*>, Any>) {
     if (FX.dicontainer != null) throw TestHelperError("DI already configured")
     FX.dicontainer = object : DIContainer {
+        val main = insulator.di.DIContainer()
+
         @Suppress("UNCHECKED_CAST", "IMPLICIT_CAST_TO_ANY")
         override fun <T : Any> getInstance(type: KClass<T>): T =
-            dependencyMap.toMap()[type] as? T
-                ?: throw IllegalArgumentException("Missing dependency in test DI ${type.qualifiedName}")
+            dependencyMap.toMap()[type] as? T ?: main.getInstance(type)
     }
+}
+
+fun waitPrimaryStage(limit: Int = 20): Stage {
+    repeat(limit) {
+        val primaryStage = FX.getPrimaryStage()
+        if (primaryStage?.isShowing == true && primaryStage.isFocused) {
+            Thread.sleep(1000)
+            return primaryStage
+        }
+        Thread.sleep(1000)
+    }
+    throw TestHelperError("Timeout waiting for primary stage to show up")
 }
 
 fun cleanupDi() {
