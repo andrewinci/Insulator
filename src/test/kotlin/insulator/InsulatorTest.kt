ea5dca8bc308ac0d2eb8045428b299b87e13b281
darka91 <andrewinci@gmail.com>
2020-09-20 11:29:10 +0100

    Add unit test

--- src/test/kotlin/insulator/InsulatorTest.kt
+++ src/test/kotlin/insulator/InsulatorTest.kt
@@ -6,10 +6,17 @@ import insulator.di.DIContainer
 import insulator.lib.helpers.runOnFXThread
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.verify
+import javafx.collections.FXCollections
+import javafx.stage.Stage
+import javafx.stage.Window
 import tornadofx.* // ktlint-disable no-wildcard-imports
 
 class InsulatorTest : FunSpec({
-    test("Stop is called when the primary stage is closed") {
+    test("Set the primary stage onClose handler") {
         // arrange
         val sut = Insulator()
 
@@ -18,6 +25,26 @@ class InsulatorTest : FunSpec({
             sut.start(FX.primaryStage)
             // assert
             FX.primaryStage.onCloseRequestProperty().value shouldNotBe null
+        }
+    }
+
+    test("On stop all windows are closed") {
+        // arrange
+        val sut = Insulator()
+        val mockWindows = (1..10).map { mockk<Stage>(relaxed = true) }
+        runOnFXThread {
+            sut.start(FX.primaryStage)
+            mockkStatic(Window::class)
+            every { Window.getWindows() } returns FXCollections.observableArrayList(mockWindows)
+
+            // act
+            sut.stop()
+
+            // assert
+            verify(exactly = 1) { Window.getWindows() }
+            mockWindows.forEach {
+                verify(exactly = 1) { it.close() }
+            }
         }
     }
 
