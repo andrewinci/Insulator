7e244dbf647a71073dd36bb1b603d6d35c869c27
darka91 <andrewinci@gmail.com>
2020-09-25 06:54:56 +0100

    Improve ProducerViewModelTest

--- src/test/kotlin/insulator/viewmodel/main/topic/ProducerViewModelTest.kt
+++ src/test/kotlin/insulator/viewmodel/main/topic/ProducerViewModelTest.kt
@@ -1,6 +1,7 @@
 package insulator.viewmodel.main.topic
 
 import arrow.core.left
+import arrow.core.right
 import helper.cleanupFXFramework
 import helper.configureDi
 import helper.configureFXFramework
@@ -10,6 +11,7 @@ import insulator.lib.kafka.Producer
 import insulator.lib.kafka.StringProducer
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.mockk.every
 import io.mockk.mockk
 
@@ -17,23 +19,80 @@ class ProducerViewModelTest : FunSpec({
 
     val errorMessage = "Example error"
 
-    test("producer show a validation error if the message is invalid") {
+    test("string producer show a validation error if the message is invalid") {
         // arrange
-        val sut = ProducerViewModel("test-topic")
-        // act
-        val res = sut.valueProperty.set("test")
-        // assert
-        sut.validationErrorProperty.value shouldBe errorMessage
-    }
-
-    beforeTest {
-        configureFXFramework()
         configureDi(
             Cluster::class to Cluster.empty(),
             StringProducer::class to mockk<Producer> {
                 every { validate(any(), any()) } returns JsonToAvroException(errorMessage).left()
             }
         )
+        val sut = ProducerViewModel("test-topic")
+        // act
+        sut.valueProperty.set("test")
+        // assert
+        sut.validationErrorProperty.value shouldBe errorMessage
+        sut.canSendProperty.value shouldBe false
+    }
+
+    test("send message without key fails") {
+        // arrange
+        configureDi(
+            Cluster::class to Cluster.empty(),
+            StringProducer::class to mockk<Producer> {
+                every { validate(any(), any()) } returns Unit.right()
+            }
+        )
+        val sut = ProducerViewModel("test-topic")
+        sut.valueProperty.set("test")
+        sut.keyProperty.set(null)
+        // act
+        sut.send()
+        // assert
+        sut.error.value shouldBe Exception("Invalid key. Key must be not empty")
+        sut.canSendProperty.value shouldBe false
+    }
+
+    test("send happy path") {
+        // arrange
+        configureDi(
+            Cluster::class to Cluster.empty(),
+            StringProducer::class to mockk<Producer> {
+                every { validate(any(), any()) } returns Unit.right()
+                every { produce(any(), any(), any()) } returns Unit.right()
+            }
+        )
+        val sut = ProducerViewModel("test-topic")
+        sut.valueProperty.set("test")
+        sut.keyProperty.set("test")
+        // act
+        sut.send()
+        // assert
+        sut.error.value shouldBe null
+        sut.canSendProperty.value shouldBe true
+    }
+
+    test("show an error if send fails") {
+        // arrange
+        configureDi(
+            Cluster::class to Cluster.empty(),
+            StringProducer::class to mockk<Producer> {
+                every { validate(any(), any()) } returns Unit.right()
+                every { produce(any(), any(), any()) } returns Throwable("sample").left()
+            }
+        )
+        val sut = ProducerViewModel("test-topic")
+        sut.valueProperty.set("test")
+        sut.keyProperty.set("test")
+        // act
+        sut.send()
+        // assert
+        sut.error.value shouldNotBe null
+        sut.canSendProperty.value shouldBe true
+    }
+
+    beforeTest {
+        configureFXFramework()
     }
     afterTest {
         cleanupFXFramework()
