fb5bcd6f0520c76c2b2279f2629b05569353dd42
darka91 <andrewinci@gmail.com>
2020-09-28 07:30:08 +0100

    Reduce MCC for SimpleTypeParsersFactory

--- src/main/kotlin/insulator/lib/jsonhelper/jsontoavro/fieldparser/SimpleTypeParsers.kt
+++ src/main/kotlin/insulator/lib/jsonhelper/jsontoavro/fieldparser/SimpleTypeParsers.kt
@@ -19,55 +19,63 @@ data class SimpleTypeParsers(
 )
 
 class SimpleTypeParsersFactory {
-    fun build() = SimpleTypeParsers(
-        intParser = jsonFieldParser { field, schema ->
-            if (field is Int) field.right()
-            else JsonInvalidFieldException(schema, field).left()
-        },
-        longParser = jsonFieldParser { field, schema ->
-            when (field) {
-                is Long -> field.right()
-                is Int -> field.toLong().right()
-                else -> JsonInvalidFieldException(schema, field).left()
-            }
-        },
-        floatParser = jsonFieldParser { field, schema ->
-            when (field) {
-                is Float -> field.right()
-                is Double -> field.toFloat().right()
-                else -> JsonInvalidFieldException(schema, field).left()
-            }
-        },
-        doubleParser = jsonFieldParser { field, schema ->
-            when (field) {
-                is Double -> field.right()
-                is Float -> field.toDouble().right()
-                else -> JsonInvalidFieldException(schema, field).left()
-            }
-        },
-        nullParser = jsonFieldParser { field, schema ->
-            if (field == null) field.right()
-            else JsonInvalidFieldException(schema, field).left()
-        },
-        booleanParser = jsonFieldParser { field, schema ->
-            when (field) {
-                is Boolean -> field.right()
-                is String -> when (field.toLowerCase()) {
-                    "false" -> false.right()
-                    "true" -> true.right()
-                    else -> JsonInvalidFieldException(schema, field).left()
-                }
-                else -> JsonInvalidFieldException(schema, field).left()
-            }
-        },
-        stringParser = jsonFieldParser { field, schema ->
-            if (field is String) field.right()
-            else JsonInvalidFieldException(schema, field).left()
-        },
-        enumParser = jsonFieldParser { field, schema ->
-            val symbols = schema.enumSymbols
-            if (field == null || field.toString() !in symbols) JsonInvalidFieldException(schema, field).left()
-            else GenericData.EnumSymbol(schema, field).right()
+
+    fun build() = SimpleTypeParsers(intParser, longParser, floatParser, doubleParser, nullParser, booleanParser, enumParser, stringParser)
+
+    private val intParser = jsonFieldParser { field, schema ->
+        if (field is Int) field.right()
+        else JsonInvalidFieldException(schema, field).left()
+    }
+
+    private val longParser = jsonFieldParser { field, schema ->
+        when (field) {
+            is Long -> field.right()
+            is Int -> field.toLong().right()
+            else -> JsonInvalidFieldException(schema, field).left()
         }
-    )
+    }
+
+    private val floatParser = jsonFieldParser { field, schema ->
+        when (field) {
+            is Float -> field.right()
+            is Double -> field.toFloat().right()
+            else -> JsonInvalidFieldException(schema, field).left()
+        }
+    }
+
+    private val doubleParser = jsonFieldParser { field, schema ->
+        when (field) {
+            is Double -> field.right()
+            is Float -> field.toDouble().right()
+            else -> JsonInvalidFieldException(schema, field).left()
+        }
+    }
+
+    private val nullParser = jsonFieldParser { field, schema ->
+        if (field == null) field.right()
+        else JsonInvalidFieldException(schema, field).left()
+    }
+
+    private val booleanParser = jsonFieldParser { field, schema ->
+        when (field) {
+            is Boolean -> field.right()
+            is String -> when (field.toLowerCase()) {
+                "false" -> false.right()
+                "true" -> true.right()
+                else -> JsonInvalidFieldException(schema, field).left()
+            }
+            else -> JsonInvalidFieldException(schema, field).left()
+        }
+    }
+
+    private val stringParser = jsonFieldParser { field, schema ->
+        if (field is String) field.right()
+        else JsonInvalidFieldException(schema, field).left()
+    }
+
+    private val enumParser = jsonFieldParser { field, schema ->
+        val symbols = schema.enumSymbols
+        if (field == null || field.toString() !in symbols) JsonInvalidFieldException(schema, field).left()
+        else GenericData.EnumSymbol(schema, field).right()
+    }
 }
