e2a18d253bcc1bd802f76faa1702a7be74795329
darka91 <andrewinci@gmail.com>
2020-10-01 06:18:22 +0100

    Refactor the avro2json converter

--- src/main/kotlin/insulator/lib/jsonhelper/AvroToJsonConverter.kt
+++ src/main/kotlin/insulator/lib/jsonhelper/AvroToJsonConverter.kt
@@ -9,6 +9,7 @@ import insulator.lib.helpers.toEither
 import insulator.lib.helpers.toEitherOfList
 import org.apache.avro.Conversions
 import org.apache.avro.Schema
+import org.apache.avro.Schema.Type.* // ktlint-disable no-wildcard-imports
 import org.apache.avro.generic.GenericData
 import org.apache.avro.generic.GenericRecord
 import org.apache.avro.util.Utf8
@@ -16,54 +17,34 @@ import java.nio.ByteBuffer
 
 open class AvroToJsonParsingException(message: String) : Throwable(message)
 class AvroFieldParsingException(field: Any?, type: String) : AvroToJsonParsingException("Invalid field $field. Expected $type")
+class UnsupportedTypeException(type: String) : AvroToJsonParsingException("Unsupported $type")
 
 class AvroToJsonConverter(private val objectMapper: ObjectMapper) {
 
     fun parse(record: GenericRecord) =
         parseField(record, record.schema)
-            .flatMap {
-                objectMapper.runCatching { writeValueAsString(it) }.toEither { AvroToJsonParsingException("Unable to write the json") }
-            }
+            .map { objectMapper.runCatching { writeValueAsString(it) } }
+            .flatMap { it.toEither { AvroToJsonParsingException("Unable to write the json") } }
 
-    private fun parseField(field: Any?, schema: Schema): Either<AvroFieldParsingException, Any?> {
-        return when (schema.type) {
-            Schema.Type.RECORD -> parseRecord(field, schema)
-            Schema.Type.BYTES -> parseBytes(field, schema)
-            Schema.Type.UNION -> parseUnion(field, schema)
-            Schema.Type.ARRAY -> parseArray(field, schema)
-            Schema.Type.NULL -> parseNull(field)
-            Schema.Type.INT, Schema.Type.LONG, Schema.Type.FLOAT, Schema.Type.DOUBLE -> parseNumber(field)
-            Schema.Type.BOOLEAN -> parseBoolean(field)
-            Schema.Type.STRING -> parseString(field)
-            Schema.Type.ENUM -> parseEnum(field)
-// todo            Schema.Type.MAP -> checkType<String>()
-// todo            Schema.Type.FIXED -> checkType<String>()
+    private fun parseField(field: Any?, schema: Schema): Either<AvroToJsonParsingException, Any?> =
+        when (schema.type) {
+            RECORD -> parseRecord(field, schema)
+            BYTES -> parseBytes(field, schema)
+            UNION -> parseUnion(field, schema)
+            ARRAY -> parseArray(field, schema)
+            NULL -> parseNull(field)
+            BOOLEAN -> parseBoolean(field)
+            STRING -> parseString(field)
+            ENUM -> parseEnum(field)
+            INT, LONG, FLOAT, DOUBLE -> parseNumber(field)
+
+            MAP -> UnsupportedTypeException("MAP").left()
+            FIXED -> UnsupportedTypeException("FIXED").left()
 
             else -> field.toString().right()
         }
-    }
 
-    private fun parseBoolean(field: Any?) =
-        if (field is Boolean) field.right() else AvroFieldParsingException(field, "Boolean").left()
-
-    private fun parseNull(field: Any?) =
-        if (field == null) null.right() else AvroFieldParsingException(field, "Null").left()
-
-    private fun parseNumber(field: Any?) =
-        if (field is Number) field.right()
-        else AvroFieldParsingException(field, "Number").left()
-
-    private fun parseEnum(field: Any?) =
-        (field as? GenericData.EnumSymbol)?.toString()?.right()
-            ?: AvroFieldParsingException(field, "Enum").left()
-
-    private fun parseString(field: Any?) =
-        when (field) {
-            is String, is Utf8 -> field.toString().right()
-            else -> AvroFieldParsingException(field, "String").left()
-        }
-
-    private fun parseRecord(field: Any?, schema: Schema): Either<AvroFieldParsingException, Any?> {
+    private fun parseRecord(field: Any?, schema: Schema): Either<AvroToJsonParsingException, Any?> {
         if (field !is GenericRecord) return AvroFieldParsingException(field, "Record").left()
         val keySchema = schema.fields.map { it.name() to it.schema() }
         return keySchema
@@ -72,19 +53,33 @@ class AvroToJsonConverter(private val objectMapper: ObjectMapper) {
             .map { values -> keySchema.map { it.first }.zip(values).toMap() }
     }
 
-    private fun parseArray(field: Any?, schema: Schema): Either<AvroFieldParsingException, Any?> {
-        if (field !is GenericData.Array<*>) return AvroFieldParsingException(field, "Array").left()
-        return field.map { parseField(it, schema.elementType) }.toEitherOfList()
-    }
+    private fun parseUnion(field: Any?, schema: Schema) =
+        schema.types.map { t -> parseField(field, t) }
+            .let { attempts -> attempts.firstOrNull { it.isRight() } ?: attempts.first() }
 
-    private fun parseUnion(field: Any?, schema: Schema): Either<AvroFieldParsingException, Any?> {
-        val mapAttempts = schema.types.map { t -> parseField(field, t) }
-        return mapAttempts.firstOrNull { it.isRight() } ?: mapAttempts.first()
-    }
+    private fun parseArray(field: Any?, schema: Schema) =
+        if (field is GenericData.Array<*>) field.map { parseField(it, schema.elementType) }.toEitherOfList()
+        else AvroFieldParsingException(field, "Array").left()
 
-    private fun parseBytes(field: Any?, schema: Schema): Either<AvroFieldParsingException, Any?> {
-        if (field !is ByteBuffer) return AvroFieldParsingException(field, "ByteBuffer").left()
-        return if (schema.objectProps["logicalType"] == "decimal") Conversions.DecimalConversion().fromBytes(field, schema, schema.logicalType).right()
-        else ("0x" + field.array().joinToString("") { String.format("%02x", it) }).right()
-    }
+    private fun parseBoolean(field: Any?) =
+        if (field is Boolean) field.right() else AvroFieldParsingException(field, "Boolean").left()
+
+    private fun parseNumber(field: Any?) =
+        if (field is Number) field.right() else AvroFieldParsingException(field, "Number").left()
+
+    private fun parseEnum(field: Any?) =
+        if (field is GenericData.EnumSymbol) field.toString().right() else AvroFieldParsingException(field, "Enum").left()
+
+    private fun parseString(field: Any?) =
+        if (field is String || field is Utf8) field.toString().right() else AvroFieldParsingException(field, "String").left()
+
+    private fun parseNull(field: Any?) =
+        if (field == null) null.right() else AvroFieldParsingException(field, "Null").left()
+
+    private fun parseBytes(field: Any?, schema: Schema) =
+        when {
+            field !is ByteBuffer -> AvroFieldParsingException(field, "ByteBuffer").left()
+            schema.objectProps["logicalType"] == "decimal" -> Conversions.DecimalConversion().fromBytes(field, schema, schema.logicalType).right()
+            else -> ("0x" + field.array().joinToString("") { String.format("%02x", it) }).right()
+        }
 }
