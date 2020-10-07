a29aea3283d90da13bd5852ec26de8cabc1d1a72
andrea-vinci <andrewinci@gmail.com>
2020-10-07 06:02:16 +0100

    Improve producer view

--- src/main/kotlin/insulator/views/main/topic/ProducerView.kt
+++ src/main/kotlin/insulator/views/main/topic/ProducerView.kt
@@ -5,6 +5,7 @@ import insulator.ui.component.h1
 import insulator.viewmodel.main.topic.ProducerViewModel
 import insulator.views.common.InsulatorView
 import javafx.beans.binding.Bindings
+import javafx.event.EventTarget
 import javafx.scene.control.ScrollPane
 import javafx.scene.control.TextArea
 import javafx.scene.layout.Priority
@@ -13,45 +14,55 @@ import tornadofx.* // ktlint-disable no-wildcard-imports
 
 class ProducerView : InsulatorView<ProducerViewModel>(viewModelClazz = ProducerViewModel::class) {
 
-    private val recordTextArea = TextArea().apply {
-        textProperty().bindBidirectional(viewModel.valueProperty)
-        vgrow = Priority.ALWAYS
-    }
+    private val recordValueTextArea = TextArea()
 
-    override val root = borderpane {
-        shortcut("CTRL+SPACE") { autoComplete() }
-        top = appBar { h1(viewModel.topicName) }
-        center = vbox(spacing = 5.0) {
-            label("Key")
-            textfield(viewModel.keyProperty)
-            recordTextArea.attachTo(this)
-            scrollpane {
-                label(viewModel.validationErrorProperty) {
-                    textFill = Color.RED
-                    isWrapText = true
-                    onDoubleClick { autoComplete() }
-                }
-                vbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
-                minHeight = 15.0
-                maxHeight = 100.0
-            }
-            borderpane {
-                paddingAll = 20.0
-                right = button("Send") {
-                    enableWhen(viewModel.canSendProperty)
-                    action {
-                        viewModel.send()
-                    }
-                }
+    override val root = vbox(spacing = 10.0) {
+        appBar { h1(viewModel.topicName) }
+
+        label("Key")
+        textfield(viewModel.keyProperty)
+
+        label("Value")
+        recordValueTextArea()
+
+        label("Validation");
+        validationArea()
+
+        borderpane {
+            right = button("Send") {
+                enableWhen(viewModel.canSendProperty)
+                action { viewModel.send() }
             }
         }
+
+        shortcut("CTRL+SPACE") { autoComplete() }
         prefWidth = 800.0
         prefHeight = 800.0
     }
 
+    private fun EventTarget.validationArea() =
+        scrollpane {
+            label(viewModel.validationErrorProperty) {
+                textFill = Color.RED
+                isWrapText = true
+                onDoubleClick { autoComplete() }
+            }
+            vbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
+            minHeight = 15.0
+            maxHeight = 100.0
+        }
+
+    private fun EventTarget.recordValueTextArea() {
+        recordValueTextArea.apply {
+            textProperty().bindBidirectional(viewModel.valueProperty)
+            vgrow = Priority.ALWAYS
+        }
+        recordValueTextArea.attachTo(this)
+    }
+
     private fun autoComplete() {
         if (!viewModel.nextFieldProperty.value.isNullOrEmpty()) {
-            with(recordTextArea) {
+            with(recordValueTextArea) {
                 insertText(caretPosition, "\"${viewModel.nextFieldProperty.value}\":")
             }
         }
