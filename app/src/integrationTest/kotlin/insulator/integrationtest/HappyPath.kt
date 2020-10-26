b7acc8ee47d366fcd96770656615e537a08162ff
Andrea Vinci <andrewinci@gmail.com>
2020-10-26 06:39:42 +0000

    Add helper to set configurations

--- app/src/integrationTest/kotlin/insulator/integrationtest/HappyPath.kt
+++ app/src/integrationTest/kotlin/insulator/integrationtest/HappyPath.kt
@@ -2,8 +2,10 @@ package insulator.integrationtest
 
 import insulator.Insulator
 import insulator.integrationtest.helpers.FxFixture
+import insulator.kafka.model.Cluster
 import io.kotest.assertions.timing.eventually
 import io.kotest.core.spec.style.StringSpec
+import kotlinx.coroutines.delay
 import org.testfx.api.FxAssert
 import org.testfx.api.FxToolkit
 import org.testfx.matcher.control.LabeledMatchers
@@ -15,9 +17,10 @@ class HappyPath : StringSpec({
     "Happy path start the app and show list clusters view" {
         FxFixture().use {
             // arrange
+            it.storeConfiguration(Cluster(name = "test cluster", endpoint = "endpoint"))
             // act
             FxToolkit.setupApplication(Insulator::class.java)
-
+            delay(60_000)
             // assert
             it.waitFXThread()
             eventually(10.seconds) {
--- app/src/integrationTest/kotlin/insulator/integrationtest/helpers/FxFixture.kt
+++ app/src/integrationTest/kotlin/insulator/integrationtest/helpers/FxFixture.kt
@@ -1,6 +1,9 @@
 package insulator.integrationtest.helpers
 
+import insulator.configuration.ConfigurationRepo
 import insulator.kafka.model.Cluster
+import insulator.test.helper.deleteTestSandboxFolder
+import insulator.test.helper.getTestSandboxFolder
 import org.testfx.api.FxToolkit
 import org.testfx.util.WaitForAsyncUtils
 import tornadofx.FX
@@ -8,11 +11,19 @@ import java.io.Closeable
 
 class FxFixture() : Closeable {
     val cluster = Cluster.empty()
+    val currentHomeFolder = getTestSandboxFolder().toAbsolutePath()
 
     init {
         val stage = FxToolkit.registerPrimaryStage()
         FX.setPrimaryStage(stage = stage)
+        // override home user to avoid messing up with current user configs
+        System.setProperty("user.home", currentHomeFolder.toString())
     }
+
+    suspend fun storeConfiguration(vararg cluster: Cluster) =
+        ConfigurationRepo("$currentHomeFolder/.insulator.config").let { repo ->
+            cluster.forEach { repo.store(it) }
+        }
 
     fun waitFXThread() {
         WaitForAsyncUtils.waitForFxEvents()
@@ -21,5 +32,6 @@ class FxFixture() : Closeable {
     override fun close() {
         waitFXThread() // wait any pending task
         FxToolkit.cleanupStages()
+        deleteTestSandboxFolder()
     }
 }
