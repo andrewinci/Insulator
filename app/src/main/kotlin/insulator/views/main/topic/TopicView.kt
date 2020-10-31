098e9c9611720d5d9c28145705de62cc867f3255
Andrea Vinci <andrewinci@gmail.com>
2020-10-31 17:19:02 +0000

    Fix open automatically open producer

--- app/src/main/kotlin/insulator/views/main/topic/TopicView.kt
+++ app/src/main/kotlin/insulator/views/main/topic/TopicView.kt
@@ -64,7 +64,7 @@ class TopicView @Inject constructor(
         borderpane {
             padding = Insets(-5.0, 0.0, 10.0, 0.0)
             left = hbox(alignment = Pos.CENTER, spacing = 5.0) {
-                button("Produce") { viewModel.showProducerView(); addClass(ButtonStyle.blueButton) }
+                button("Produce") { action { viewModel.showProducerView() }; addClass(ButtonStyle.blueButton) }
                 consumeStopButton()
                 fieldName("from")
                 consumeFromCombobox()
