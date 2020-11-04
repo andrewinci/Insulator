da450bcadec6867ee228ed196bef9ef7a9b08ee6
Auto Lint <autolint@lint.local>
2020-11-04 03:26:53 +0000

    Fix PR lint

--- app/src/integrationTest/kotlin/insulator/integrationtest/ProducerTest.kt
+++ app/src/integrationTest/kotlin/insulator/integrationtest/ProducerTest.kt
@@ -14,8 +14,6 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import javafx.scene.control.Button
 import javafx.scene.control.TableView
-import javafx.scene.control.TextArea
-import javafx.scene.control.TextField
 import javafx.scene.control.TextInputControl
 import tornadofx.CssRule
 import tornadofx.Stylesheet
