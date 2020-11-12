6348acccb547ac74f0576ef00f8a21d7004f8c6a
Andrea Vinci <andrewinci@gmail.com>
2020-11-12 04:01:23 +0000

    Add shadowJar back

--- scripts/constants.py
+++ scripts/constants.py
@@ -22,7 +22,7 @@ manual_dependencies = {
     "javafx-graphics-15.0.1-linux.jar": '<file os="linux" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/15.0.1/javafx-graphics-15.0.1-linux.jar"  size="5063475" classpath="true" checksum="46774dc6"/>',
     "javafx-base-15.0.1-linux.jar": '<file os="linux" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-base/15.0.1/javafx-base-15.0.1-linux.jar"  size="745527" classpath="true" checksum="d6bedd4d"/>',
     "javafx-controls-15.0.1-linux.jar": '<file os="linux" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-controls/15.0.1/javafx-controls-15.0.1-linux.jar"  size="2531099" classpath="true" checksum="8e5467f3"/>',
-    "javafx-fxml-15.0.1-linux.jar": '<file os="linux" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-fxml/15.0.1/javafx-fxml-15.0.1-linux.jar"  size="128790" classpath="true" checksum="ecb5193e"/>§',
+    "javafx-fxml-15.0.1-linux.jar": '<file os="linux" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-fxml/15.0.1/javafx-fxml-15.0.1-linux.jar"  size="128790" classpath="true" checksum="ecb5193e"/>',
     # javafx for windows
     "javafx-swing-15.0.1-win.jar": '<file os="win" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-swing/15.0.1/javafx-swing-15.0.1-win.jar"  size="88725" classpath="true" checksum="34bf6673"/>',
     "javafx-graphics-15.0.1-win.jar": '<file os="win" uri="https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/15.0.1/javafx-graphics-15.0.1-win.jar"  size="5898578" classpath="true" checksum="4eef746f"/>',
