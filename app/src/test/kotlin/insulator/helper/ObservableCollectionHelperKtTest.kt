99db44d0c9678bb1c1d1349ae1ebebd9690712ff
Andrea Vinci <andrewinci@gmail.com>
2020-11-16 06:44:04 +0000

    Add test for createListBindings

---
+++ app/src/test/kotlin/insulator/helper/ObservableCollectionHelperKtTest.kt
@@ -0,0 +1,36 @@
+package insulator.helper
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import javafx.beans.property.SimpleStringProperty
+import javafx.beans.property.StringProperty
+
+class ObservableCollectionHelperKtTest : FreeSpec({
+    
+    "createListBindings" - {
+        // arrange
+        val prop = SimpleStringProperty("")
+        val op = { prop.value.toCharArray().toList() }
+        // act
+        val observableCollection = createListBindings(op, prop)
+
+        "on prop change, the observable collection changes as well 1"{
+            val test = "test"
+            prop.set(test)
+            observableCollection.toList() shouldBe test.toCharArray().toList()
+        }
+
+        "on prop change, the observable collection changes as well 2"{
+            val test = "another test"
+            prop.set(test)
+            observableCollection.toList() shouldBe test.toCharArray().toList()
+        }
+    }
+
+    "createListBindings without base prop"{
+        val testList = (1..10).toList()
+        val observableCollection = createListBindings({ testList })
+        observableCollection.toList() shouldBe testList
+    }
+})
