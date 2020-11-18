147d09909594f891cd07725f66249ea7ba851911
Andrea Vinci <andrewinci@gmail.com>
2020-11-18 19:26:27 +0000

    Move topic view model to the view model package

---
+++ app/src/main/kotlin/insulator/viewmodel/main/topic/TopicInfoViewModel.kt
@@ -0,0 +1,6 @@
+package insulator.viewmodel.main.topic
+
+import insulator.kafka.model.Topic
+import insulator.viewmodel.common.InsulatorViewModel
+
+class TopicInfoViewModel(val topic: Topic) : InsulatorViewModel()
