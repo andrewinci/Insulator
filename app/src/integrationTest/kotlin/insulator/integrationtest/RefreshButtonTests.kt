86fcac644a62d18224ad51e3f10e4517579b933e
Auto Lint <autolint@lint.local>
2020-11-18 07:06:50 +0000

    Fix PR lint

--- app/src/integrationTest/kotlin/insulator/integrationtest/RefreshButtonTests.kt
+++ app/src/integrationTest/kotlin/insulator/integrationtest/RefreshButtonTests.kt
@@ -15,7 +15,6 @@ import javafx.scene.control.Button
 import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
 import javafx.scene.control.ListView
-import kotlinx.coroutines.delay
 import tornadofx.CssRule
 import tornadofx.Stylesheet.Companion.listView
 import tornadofx.Stylesheet.Companion.text
