f1642294275940607a888690dc7817d2626f6095
Andrea Vinci <andrewinci@gmail.com>
2020-11-20 04:01:59 +0000

    Fix validation in the edit cluster view

--- app/src/main/kotlin/insulator/views/configurations/ClusterView.kt
+++ app/src/main/kotlin/insulator/views/configurations/ClusterView.kt
@@ -5,6 +5,7 @@ import insulator.ui.component.confirmationButton
 import insulator.ui.component.h1
 import insulator.viewmodel.configurations.ClusterViewModel
 import javafx.beans.binding.Bindings
+import javafx.beans.property.BooleanProperty
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.value.ObservableValue
 import javafx.event.EventTarget
@@ -22,8 +23,6 @@ import tornadofx.enableWhen
 import tornadofx.field
 import tornadofx.fieldset
 import tornadofx.form
-import tornadofx.required
-import tornadofx.requiredWhen
 import tornadofx.textfield
 import javax.inject.Inject
 
@@ -35,22 +34,22 @@ class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) :
     override val root = form {
         fieldset {
             h1("Cluster connection")
-            field("Cluster name") { customTextField(viewModel.nameProperty) { id = "field-cluster-name" }.required() }
-            field("Endpoint (url:port)") { customTextField(viewModel.endpointProperty) { id = "field-endpoint" }.required() }
+            field("Cluster name") { customTextField(viewModel.nameProperty) { id = "field-cluster-name" } }
+            field("Endpoint (url:port)") { customTextField(viewModel.endpointProperty) { id = "field-endpoint" } }
             fieldset {
                 disableWhen(viewModel.useSaslProperty)
-                field("Use SSL (Aiven)") { checkbox(property = viewModel.useSSLProperty) }
-                field("SSL Truststore Location") { customTextField(viewModel.sslTruststoreLocationProperty).requiredWhen(viewModel.useSSLProperty) }
-                field("SSL Truststore Password") { customTextField(viewModel.sslTruststorePasswordProperty).requiredWhen(viewModel.useSSLProperty) }
-                field("SSL Keystore Location") { customTextField(viewModel.sslKeystoreLocationProperty).requiredWhen(viewModel.useSSLProperty) }
-                field("SSL KeyStore Password") { customTextField(viewModel.sslKeyStorePasswordProperty).requiredWhen(viewModel.useSSLProperty) }
+                field("Use SSL (Aiven)") { checkbox(property = viewModel.useSSLProperty) { enableWhen(canEdit) } }
+                field("SSL Truststore Location") { customTextField(viewModel.sslTruststoreLocationProperty, viewModel.useSSLProperty) }
+                field("SSL Truststore Password") { customTextField(viewModel.sslTruststorePasswordProperty, viewModel.useSSLProperty) }
+                field("SSL Keystore Location") { customTextField(viewModel.sslKeystoreLocationProperty, viewModel.useSSLProperty) }
+                field("SSL KeyStore Password") { customTextField(viewModel.sslKeyStorePasswordProperty, viewModel.useSSLProperty) }
             }
             fieldset {
                 disableWhen(viewModel.useSSLProperty)
-                field("Use SASL") { checkbox(property = viewModel.useSaslProperty) }
-                field("Username") { customTextField(viewModel.saslUsernameProperty).requiredWhen(viewModel.useSaslProperty) }
-                field("Password") { customTextField(viewModel.saslPasswordProperty).requiredWhen(viewModel.useSaslProperty) }
-                field("SCRAM") { checkbox(property = viewModel.useScramProperty) }
+                field("Use SASL") { checkbox(property = viewModel.useSaslProperty) { enableWhen(canEdit) } }
+                field("Username") { customTextField(viewModel.saslUsernameProperty, viewModel.useSaslProperty) }
+                field("Password") { customTextField(viewModel.saslPasswordProperty, viewModel.useSaslProperty) }
+                field("SCRAM") { checkbox(property = viewModel.useScramProperty) { enableWhen(canEdit) } }
             }
             fieldset {
                 h1("Schema registry")
@@ -59,7 +58,6 @@ class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) :
                 field("Password") { customTextField(viewModel.schemaRegistryPasswordProperty) }
             }
         }
-
         borderpane {
             left = deleteButton()
             right = saveButton()
@@ -72,8 +70,12 @@ class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) :
         this.canEdit.set(isEditable)
     }
 
-    private fun EventTarget.customTextField(property: ObservableValue<String>, op: TextField.() -> Unit = {}) =
-        textfield(property) { editableWhen(canEdit); op() }
+    private fun EventTarget.customTextField(property: ObservableValue<String>, dependency: BooleanProperty? = null, op: TextField.() -> Unit = {}) =
+        textfield(property) {
+            editableWhen(canEdit)
+            if (dependency != null) enableWhen(dependency)
+            op()
+        }
 
     private fun EventTarget.deleteButton() = confirmationButton(
         "Delete",
@@ -87,12 +89,9 @@ class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) :
     private fun EventTarget.saveButton() =
         button {
             textProperty().bind(Bindings.createStringBinding({ if (canEdit.value) "Save" else "Close" }, canEdit))
-            enableWhen(viewModel.valid)
+            enableWhen(viewModel.isValidProperty)
             action {
-                if (text == "Save") {
-                    viewModel.commit()
-                    viewModel.dispatch { save() }
-                }
+                if (text == "Save") viewModel.dispatch { save() }
                 close()
             }
         }
