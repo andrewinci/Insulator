ae6439ce18625cf9877a3f045e312fd9600dfdd6
Andrea Vinci <andrewinci@gmail.com>
2020-11-20 04:19:31 +0000

    Add select file in cluster config

--- app/src/main/kotlin/insulator/views/configurations/ClusterView.kt
+++ app/src/main/kotlin/insulator/views/configurations/ClusterView.kt
@@ -5,11 +5,12 @@ import insulator.ui.component.confirmationButton
 import insulator.ui.component.h1
 import insulator.viewmodel.configurations.ClusterViewModel
 import javafx.beans.binding.Bindings
-import javafx.beans.property.BooleanProperty
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.StringProperty
 import javafx.beans.value.ObservableValue
 import javafx.event.EventTarget
 import javafx.scene.control.TextField
+import javafx.stage.FileChooser
 import javafx.stage.Modality
 import javafx.stage.StageStyle
 import tornadofx.View
@@ -17,13 +18,13 @@ import tornadofx.action
 import tornadofx.borderpane
 import tornadofx.button
 import tornadofx.checkbox
-import tornadofx.disableWhen
 import tornadofx.editableWhen
 import tornadofx.enableWhen
 import tornadofx.field
 import tornadofx.fieldset
 import tornadofx.form
 import tornadofx.textfield
+import java.io.File
 import javax.inject.Inject
 
 class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) : View() {
@@ -36,19 +37,25 @@ class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) :
             h1("Cluster connection")
             field("Cluster name") { customTextField(viewModel.nameProperty) { id = "field-cluster-name" } }
             field("Endpoint (url:port)") { customTextField(viewModel.endpointProperty) { id = "field-endpoint" } }
+            field("Use SSL (Aiven)") { checkbox(property = viewModel.useSSLProperty) { enableWhen(canEdit) } }
             fieldset {
-                disableWhen(viewModel.useSaslProperty)
-                field("Use SSL (Aiven)") { checkbox(property = viewModel.useSSLProperty) { enableWhen(canEdit) } }
-                field("SSL Truststore Location") { customTextField(viewModel.sslTruststoreLocationProperty, viewModel.useSSLProperty) }
-                field("SSL Truststore Password") { customTextField(viewModel.sslTruststorePasswordProperty, viewModel.useSSLProperty) }
-                field("SSL Keystore Location") { customTextField(viewModel.sslKeystoreLocationProperty, viewModel.useSSLProperty) }
-                field("SSL KeyStore Password") { customTextField(viewModel.sslKeyStorePasswordProperty, viewModel.useSSLProperty) }
+                enableWhen(viewModel.useSSLProperty)
+                field("SSL Truststore Location") {
+                    customTextField(viewModel.sslTruststoreLocationProperty) { enableWhen(SimpleBooleanProperty(false)) }
+                    fileChooser(viewModel.sslTruststoreLocationProperty)
+                }
+                field("SSL Truststore Password") { customTextField(viewModel.sslTruststorePasswordProperty) }
+                field("SSL Keystore Location") {
+                    customTextField(viewModel.sslKeystoreLocationProperty) { enableWhen(SimpleBooleanProperty(false)) }
+                    fileChooser(viewModel.sslKeystoreLocationProperty)
+                }
+                field("SSL KeyStore Password") { customTextField(viewModel.sslKeyStorePasswordProperty) }
             }
+            field("Use SASL") { checkbox(property = viewModel.useSaslProperty) { enableWhen(canEdit) } }
             fieldset {
-                disableWhen(viewModel.useSSLProperty)
-                field("Use SASL") { checkbox(property = viewModel.useSaslProperty) { enableWhen(canEdit) } }
-                field("Username") { customTextField(viewModel.saslUsernameProperty, viewModel.useSaslProperty) }
-                field("Password") { customTextField(viewModel.saslPasswordProperty, viewModel.useSaslProperty) }
+                enableWhen(viewModel.useSaslProperty)
+                field("Username") { customTextField(viewModel.saslUsernameProperty) }
+                field("Password") { customTextField(viewModel.saslPasswordProperty) }
                 field("SCRAM") { checkbox(property = viewModel.useScramProperty) { enableWhen(canEdit) } }
             }
             fieldset {
@@ -70,10 +77,21 @@ class ClusterView @Inject constructor(private val viewModel: ClusterViewModel) :
         this.canEdit.set(isEditable)
     }
 
-    private fun EventTarget.customTextField(property: ObservableValue<String>, dependency: BooleanProperty? = null, op: TextField.() -> Unit = {}) =
+    private fun EventTarget.fileChooser(prop: StringProperty) = button("Select file") {
+        action {
+            FileChooser().apply {
+                prop.value?.let {
+                    val selected = File(it)
+                    initialDirectory = selected.parentFile
+                    initialFileName = selected.name
+                }
+            }.showOpenDialog(currentWindow)?.absolutePath?.let { prop.set(it) }
+        }
+    }
+
+    private fun EventTarget.customTextField(property: ObservableValue<String>, op: TextField.() -> Unit = {}) =
         textfield(property) {
             editableWhen(canEdit)
-            if (dependency != null) enableWhen(dependency)
             op()
         }
 
