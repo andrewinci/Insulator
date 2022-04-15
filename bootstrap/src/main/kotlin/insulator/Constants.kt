5e5419b1b66c3316c4cdad86cf829fe464feafb9
Andrea <55388041+andrewinci@users.noreply.github.com>
2022-04-15 08:04:08 +0100

    feat: Allow to use a specific version instead of the latest (#244)

--- bootstrap/src/main/kotlin/insulator/Constants.kt
+++ bootstrap/src/main/kotlin/insulator/Constants.kt
@@ -2,17 +2,18 @@ package insulator
 
 import java.nio.file.Path
 
-const val appName = "Insulator"
-val localPath: String by lazy {
+const val APP_NAME = "Insulator"
+val LOCAL_CONFIG_PATH: String by lazy {
     with(System.getProperty("os.name")) {
         when {
-            contains("nux") -> "${System.getProperty("user.home")!!}/.config/$appName/"
-            contains("win") -> "${System.getenv("LOCALAPPDATA")}/$appName"
-            else -> "${System.getProperty("user.home")!!}/Library/Application Support/$appName/"
+            contains("nux") -> "${System.getProperty("user.home")!!}/.config/$APP_NAME/"
+            contains("win") -> "${System.getenv("LOCALAPPDATA")}/$APP_NAME"
+            else -> "${System.getProperty("user.home")!!}/Library/Application Support/$APP_NAME/"
         }
     }
 }
 
-val localConfigFile = "${localPath}insulator-update.xml"
-val updatePath: Path = Path.of(System.getProperty("java.io.tmpdir") ?: "", "update.zip")
-const val configPath = "https://github.com/andrewinci/Insulator/releases/latest/download/insulator-update.xml"
+val LOCAL_CONFIG_FILE_PATH = "${LOCAL_CONFIG_PATH}insulator-update.xml"
+val TMP_UPDATE_PATH: Path = Path.of(System.getProperty("java.io.tmpdir") ?: "", "update.zip")
+val DEV_FILE_PATH = "${LOCAL_CONFIG_PATH}DEV_MODE"
+const val LATEST_RELEASE_URL = "https://github.com/andrewinci/Insulator/releases/latest/download/insulator-update.xml"
